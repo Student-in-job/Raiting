9fdd115f8a6c3ff80443b9f94ed9a9ac730b78e3
Andronik <test13r@gmail.com>
2015-09-22 17:03:57 +0500

    New project`s files.

---
+++ RatingUniversity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RatingUniversity.MvcApplication" Language="C#" %>
---
+++ RatingUniversity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RatingUniversity.Startup))]
+namespace RatingUniversity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
