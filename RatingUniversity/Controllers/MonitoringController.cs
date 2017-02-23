ee65dc9d667c7a9a522df51f1ae369f735f9ea23
vitr-admin@virt-srv.local <vitr-admin@virt-srv.local>
2017-02-23 13:51:41 +0500

    add new Monitoring/Index.cshtml change old Monitoring/Index.cshtml to Monitoring/Status.cshtml

--- RatingUniversity/Controllers/MonitoringController.cs
+++ RatingUniversity/Controllers/MonitoringController.cs
@@ -1,19 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using RatingUniversity.Models;
+using RatingUniversity.Classes;
+using System.Data.Entity;
 
 namespace RatingUniversity.Controllers
 {
     public class MonitoringController : Controller
     {
+        TablesContext db = new TablesContext();
         //
         // GET: /Monitoring/
         public ActionResult Index()
         {
-			TablesContext db = new TablesContext();
+            ViewBag.Regions = this.db.region.ToList();
+            return View(this.db.university.OrderBy(m => m.id).ToList());
+        }
+
+         //
+        // GET: /Monitoring/Status
+        public ActionResult Status()
+        {
 			int yil = Int32.Parse(DateTime.Now.Year.ToString());
 			var list = db.Monitorings.Where(pr => pr.Year == yil).OrderBy(j => j.Year).ToList();
 			ViewBag.bor = true;
